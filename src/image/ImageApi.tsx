deb8bbdfdeaf98b23f7d221887ce1078a1839e2a
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-27 13:29:55 -0300

    fixed

--- src/image/ImageApi.tsx
+++ src/image/ImageApi.tsx
@@ -4,58 +4,67 @@ import { environment } from "../system/environment/environment";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 export enum Quality {
-    Q160 = "160",
-    Q320 = "320",
-    Q640 = "640",
-    Q800 = "800",
-    Q1024 = "1024",
-    Q1200 = "1200",
+  Q160 = "160",
+  Q320 = "320",
+  Q640 = "640",
+  Q800 = "800",
+  Q1024 = "1024",
+  Q1200 = "1200",
 }
 
 export interface IImage {
-    id?: string;
-    image: string;
+  id?: string;
+  image: string;
 }
 
-export async function getImage(id: string, quality?: Quality, jpeg: boolean = false): Promise<IImage> {
-    try {
-        const headers = quality ? {
-            headers: {
-                Size: quality,
-            },
-        } : undefined;
-
-        const url = environment.imageServerUrl + "image/" + id;
-        if (jpeg) {
-            return Promise.resolve({
-                image: url + "/jpeg?Size=" + quality,
-            });
+export async function getImage(
+  id: string,
+  quality?: Quality,
+  jpeg: boolean = false
+): Promise<IImage> {
+  try {
+    const headers = quality
+      ? {
+          headers: {
+            Size: quality,
+          },
         }
+      : undefined;
 
-        const res = await axios.get(url, headers);
-        return Promise.resolve(res.data);
-    } catch (err) {
-        return Promise.reject(err);
+    const url = environment.imageServerUrl + "images/" + id;
+    if (jpeg) {
+      return Promise.resolve({
+        image: url + "/jpeg?Size=" + quality,
+      });
     }
+
+    const res = await axios.get(url, headers);
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export async function saveImage(value: IImage): Promise<IImage> {
-    try {
-        const res = await axios.post(environment.imageServerUrl + "image", value);
-        return Promise.resolve(res.data);
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  try {
+    const res = await axios.post(
+      environment.imageServerUrl + "images/create",
+      value
+    );
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export function getPictureUrl(id: string | undefined) {
-    if (id && id.length > 0) {
-        if (id.length > 100) {
-            return id;
-        } else {
-            return environment.imageServerUrl + "image/" + id + "/jpeg?Size=160";
-        }
+  if (id && id.length > 0) {
+    if (id.length > 100) {
+      return id;
     } else {
-        return "/assets/select_image.png";
+      return environment.imageServerUrl + "images/" + id + "/jpeg?Size=160";
     }
+  } else {
+    return "/assets/select_image.png";
+  }
 }
--- src/image/ShowImage.tsx
+++ src/image/ShowImage.tsx
@@ -1,36 +1,37 @@
-import React from "react";
 import "../styles.css";
+import { environment } from "../system/environment/environment";
 import { DefaultProps } from "../system/utils/Tools";
 import { Quality } from "./ImageApi";
 
 export interface ShowImageProps extends DefaultProps {
-    imageId?: string;
-    quality?: Quality;
+  imageId?: string;
+  quality?: Quality;
 }
 
 export default function ShowImage(props: ShowImageProps) {
-    const getImageUrl = () => {
-        if (props.imageId === undefined) {
-            return "/assets/profile.png"
-        }
-
-        let result = "http://localhost:3001/v1/image/" + props.imageId + "/jpeg"
-        if (props.quality != null) {
-            result += "?Size=" + props.quality
-        }
-        return result
+  const getImageUrl = () => {
+    if (props.imageId === undefined) {
+      return "/assets/profile.png";
     }
 
-    if (!props.imageId) {
-        return null
+    let result =
+      environment.imageServerUrl + "images/" + props.imageId + "/jpeg";
+    if (props.quality != null) {
+      result += "?Size=" + props.quality;
     }
-    return (
-        <div >
-            <img alt="" src={getImageUrl()} />
-            <div className="text-block">
-                <strong>{props.quality ? props.quality : "Original"}</strong>
-            </div>
-            <br />
-        </div>
-    );
+    return result;
+  };
+
+  if (!props.imageId) {
+    return null;
+  }
+  return (
+    <div>
+      <img alt="" src={getImageUrl()} />
+      <div className="text-block">
+        <strong>{props.quality ? props.quality : "Original"}</strong>
+      </div>
+      <br />
+    </div>
+  );
 }
