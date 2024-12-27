3e1d7568c888db450c4cfced2364b8d83b932d01
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-27 15:50:34 -0300

    Update environment.ts

--- src/system/environment/environment.ts
+++ src/system/environment/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   authServerUrl: process.env.AUTH_SERVICE_URL || "http://localhost:3000/",
-  cartServerUrl: process.env.CART_SERVICE_URL || "http://localhost:3003/v1/",
-  catalogServerUrl:
-    process.env.CATALOG_SERVICE_URL || "http://localhost:3002/",
+  cartServerUrl: process.env.CART_SERVICE_URL || "http://localhost:3003/",
+  catalogServerUrl: process.env.CATALOG_SERVICE_URL || "http://localhost:3002/",
   imageServerUrl: process.env.IMAGE_SERVICE_URL || "http://localhost:3001/",
   orderServerUrl: process.env.ORDER_SERVICE_URL || "http://localhost:3004/v1/",
   production: false,
